7b0b681fe8786dddfb9b1171c9399316727341cc
Higa <evandro_higa@hotmail.com>
2021-09-13 18:03:55 -0300

    adicao de user padrao admin no script.sql

--- docker/script.sql
+++ docker/script.sql
@@ -2,14 +2,6 @@ CREATE DATABASE IF NOT EXISTS car_cred_fiap;
 
 USE car_cred_fiap;
 
-CREATE TABLE alunos (
-	id BIGINT NOT NULL AUTO_INCREMENT,
-	nome VARCHAR(255),
-	rm VARCHAR(7),
-	cod VARCHAR(6),
-	PRIMARY KEY (id)
-);
-
 CREATE TABLE usuarios (
 	id BIGINT NOT NULL AUTO_INCREMENT,
 	username VARCHAR(255) UNIQUE NOT NULL,
@@ -17,4 +9,15 @@ CREATE TABLE usuarios (
 	role VARCHAR(5) NOT NULL,
 	PRIMARY KEY (id),
 	CONSTRAINT chk_role CHECK(role='USER' OR role='ADMIN')
+);
+
+INSERT INTO usuarios (username, password, role)
+    VALUES ('root', '$2a$10$1B6VMXGSzPjVFNXFRKlZ3.BLjhlpndmhyvtVWnDdQeZ9mUnxvYp2m', 'ADMIN');
+
+CREATE TABLE alunos (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(255),
+	rm VARCHAR(7),
+	cod VARCHAR(6),
+	PRIMARY KEY (id)
 );
